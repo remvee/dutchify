4a14a1a960bada4648fbd517e0b71ea5b2219a91
remco <remco@cb8d8d85-131c-0410-adf4-82ea522b99ed>
2006-08-30 14:37:00 +0000

    human_size, number_to_percentage, number_with_delimiter
    git-svn-id: http://svn.remvee.net/plugins/dutchify@8 cb8d8d85-131c-0410-adf4-82ea522b99ed

--- lib/number_helper.rb
+++ lib/number_helper.rb
@@ -16,6 +16,42 @@ module ActionView
         english_number_to_currency(number, options)
       end
       alias :number_to_currency :dutch_number_to_currency
+
+
+      number_to_human_size = instance_method(:number_to_human_size)
+      define_method(:english_number_to_human_size) do |*args|
+        number_to_human_size.bind(self).call(*args)
+      end
+      alias :english_human_size :english_number_to_human_size
+
+      def dutch_number_to_human_size(size)
+        english_number_to_human_size(size).tr('.', ',')
+      end
+      alias :number_to_human_size :dutch_number_to_human_size
+      alias :human_size :dutch_number_to_human_size
+
+
+      number_to_percentage = instance_method(:number_to_percentage)
+      define_method(:english_number_to_percentage) do |*args|
+        number_to_percentage.bind(self).call(*args)
+      end
+
+      def dutch_number_to_percentage(number, options = {})
+        options = {:separator => ","}.merge(options)
+        english_number_to_percentage(number, options)
+      end
+      alias :number_to_percentage :dutch_number_to_percentage
+
+
+      number_with_delimiter = instance_method(:number_with_delimiter)
+      define_method(:english_number_with_delimiter) do |*args|
+        number_with_delimiter.bind(self).call(*args)
+      end
+
+      def dutch_number_with_delimiter(number, delimiter = '.')
+        english_number_with_delimiter(number, delimiter)
+      end
+      alias :number_with_delimiter :dutch_number_with_delimiter
     end
   end
 end
--- test/number_helper_test.rb
+++ test/number_helper_test.rb
@@ -5,6 +5,7 @@ require File.dirname(__FILE__) + '/../lib/number_helper'
 
 class NumberHelperTest < Test::Unit::TestCase
   include ActionView::Helpers::NumberHelper
+  include Math
   
   def test_number_to_currency
     assert_equal '&euro;3.999,99', number_to_currency(3_999.99)
@@ -12,13 +13,39 @@ class NumberHelperTest < Test::Unit::TestCase
     assert_equal '&euro;999,99', number_to_currency(999.99)
     assert_equal '&euro;1999,99', number_to_currency(1_999.99, :delimiter => '')
     assert_equal '&euro;1.999!99', number_to_currency(1_999.99, :separator => '!')
-  end
-
-  def test_english_number_to_currency
     assert_equal '$3,999.99', english_number_to_currency(3_999.99)
     assert_equal 'EURO 3,999.99', english_number_to_currency(3_999.99, :unit => 'EURO ')
     assert_equal '$999.99', english_number_to_currency(999.99)
     assert_equal '$1999.99', english_number_to_currency(1_999.99, :delimiter => '')
     assert_equal '$1,999!99', english_number_to_currency(1_999.99, :separator => '!')
+  end
+
+  def test_human_size
+    assert_equal '1,2 KB', human_size(1234)
+    assert_equal '1,2 KB', number_to_human_size(1234)
+    assert_equal '1,2 KB', dutch_number_to_human_size(1234)
+    assert_equal '1.2 KB', english_human_size(1234)
+    assert_equal '1.2 KB', english_number_to_human_size(1234)
+  end
+
+  def test_number_to_percentage
+    assert_equal '3,142%', number_to_percentage(PI)
+    assert_equal '3!142%', number_to_percentage(PI, :separator => '!')
+    assert_equal '3!14%', number_to_percentage(PI, :separator => '!', :precision => 2)
+    assert_equal '3,142%', dutch_number_to_percentage(PI)
+    assert_equal '3!142%', dutch_number_to_percentage(PI, :separator => '!')
+    assert_equal '3!14%', dutch_number_to_percentage(PI, :separator => '!', :precision => 2)
+    assert_equal '3.142%', english_number_to_percentage(PI)
+    assert_equal '3!142%', english_number_to_percentage(PI, :separator => '!')
+    assert_equal '3!14%', english_number_to_percentage(PI, :separator => '!', :precision => 2)
+  end
+
+  def test_number_with_delimiter
+    assert_equal '1.000.000', number_with_delimiter(1_000_000)
+    assert_equal '1 000 000', number_with_delimiter(1_000_000, ' ')
+    assert_equal '1.000.000', dutch_number_with_delimiter(1_000_000)
+    assert_equal '1 000 000', dutch_number_with_delimiter(1_000_000, ' ')
+    assert_equal '1,000,000', english_number_with_delimiter(1_000_000)
+    assert_equal '1 000 000', english_number_with_delimiter(1_000_000, ' ')
   end
 end
