23ca46e52e7f2dcc04baee4c5dc871109a657310
remco <remco@cb8d8d85-131c-0410-adf4-82ea522b99ed>
2006-08-30 13:17:31 +0000

    engelse versie van ordinalize beschikbaar met english_ prefix
    git-svn-id: http://svn.remvee.net/plugins/dutchify@5 cb8d8d85-131c-0410-adf4-82ea522b99ed

--- lib/inflector.rb
+++ lib/inflector.rb
@@ -1,8 +1,18 @@
+require 'active_support/inflector'
+
+# Introduce dutch variant for +ordinalize+ method.  The original english
+# version is still available with a +english_+ prefix.
 module Inflector
-  def ordinalize(n)
+  ordinalize = instance_method(:ordinalize)
+  define_method(:english_ordinalize) do |*args|
+    ordinalize.bind(self).call(*args)
+  end
+
+  def dutch_ordinalize(n)
     case n % 100
       when 2..7, 9..19 then "#{n}de"
       else "#{n}ste"
     end
   end
+  alias :ordinalize :dutch_ordinalize
 end
--- test/inflector_test.rb
+++ test/inflector_test.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'test/unit'
-require 'inflector'
+require File.dirname(__FILE__) + '/../lib/inflector'
 
 class InflectorTest < Test::Unit::TestCase
-  include Inflector
-  
   def test_ordinalize
     [
       [1, '1ste'], [2, '2de'], [3, '3de'], [8, '8ste'],
@@ -13,7 +11,15 @@ class InflectorTest < Test::Unit::TestCase
       [100, '100ste'], [101, '101ste'], [102, '102de'], [108, '108ste'], [109, '109de'],
       [2310, '2310de'], [2311, '2311de'], [2320, '2320ste'], [2321, '2321ste'],
     ].each do |t|
-      assert_equal t[1], ordinalize(t[0])
+      assert_equal t[1], Inflector.ordinalize(t[0])
+    end
+  end
+
+  def test_english_ordinalize
+    [
+      [1, '1st'], [2, '2nd'], [3, '3rd'], [4, '4th']
+    ].each do |t|
+      assert_equal t[1], Inflector.english_ordinalize(t[0])
     end
   end
 end
