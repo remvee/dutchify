0584dd500779931d3e1284d2c876a18809d5bdfc
remco <remco@cb8d8d85-131c-0410-adf4-82ea522b99ed>
2006-08-31 13:35:25 +0000

    titleize en underscore
    git-svn-id: http://svn.remvee.net/plugins/dutchify@9 cb8d8d85-131c-0410-adf4-82ea522b99ed

--- lib/inflector.rb
+++ lib/inflector.rb
@@ -1,4 +1,4 @@
-require 'active_support/inflector'
+require 'active_support'
 
 # Introduce dutch variant for +ordinalize+ method.  The original english
 # version is still available with a +english_+ prefix.
@@ -15,4 +15,46 @@ module Inflector
     end
   end
   alias :ordinalize :dutch_ordinalize
+
+
+  underscore = instance_method(:underscore)
+  define_method(:english_underscore) do |*args|
+    underscore.bind(self).call(*args)
+  end
+
+  def dutch_underscore(camel_cased_word)
+    camel_cased_word.to_s.gsub(/::/, '/').
+      gsub(/([a-z\d])(IJ|[A-Z])/, '\1_\2').
+      gsub(/([A-Z]+?)(IJ[a-z])/, '\1_\2').
+      gsub(/([A-HJ-Z]+)([A-IK-Z][a-z])/, '\1_\2').
+      tr("- ", "_").
+      downcase
+  end
+  alias :underscore :dutch_underscore
+
+
+  titleize = instance_method(:titleize)
+  define_method(:english_titleize) do |*args|
+    titleize.bind(self).call(*args)
+  end
+
+  def dutch_titleize(word)
+    humanize(underscore(word)).gsub(/\b(ij|[a-z])/i) { $1.upcase }
+  end
+  alias :titleize :dutch_titleize
+
+
+  camelize = instance_method(:camelize)
+  define_method(:english_camelize) do |*args|
+    camelize.bind(self).call(*args)
+  end
+
+  def dutch_camelize(lower_case_and_underscored_word, first_letter_in_uppercase = true)
+    if first_letter_in_uppercase
+      lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(ij|.)/) { $2.upcase }
+    else
+      lower_case_and_underscored_word.first + camelize(lower_case_and_underscored_word)[1..-1]
+    end
+  end
+  alias :camelize :dutch_camelize
 end
--- test/inflector_test.rb
+++ test/inflector_test.rb
@@ -11,15 +11,48 @@ class InflectorTest < Test::Unit::TestCase
       [100, '100ste'], [101, '101ste'], [102, '102de'], [108, '108ste'], [109, '109de'],
       [2310, '2310de'], [2311, '2311de'], [2320, '2320ste'], [2321, '2321ste'],
     ].each do |t|
-      assert_equal t[1], Inflector.ordinalize(t[0])
+      assert_equal t[1], t[0].ordinalize
     end
-  end
 
-  def test_english_ordinalize
     [
       [1, '1st'], [2, '2nd'], [3, '3rd'], [4, '4th']
     ].each do |t|
       assert_equal t[1], Inflector.english_ordinalize(t[0])
+    end
+  end
+
+  def test_underscore
+    {
+      "WijWillenIJs"                                 => "wij_willen_ijs",
+      "WijWillenSoep"                                => "wij_willen_soep",
+      "AuwSig9Gekregen"                              => "auw_sig9_gekregen",
+      "FreeBSD"                                      => "free_bsd",
+      "wij willen soep en ijsjes aan het IJsselmeer" => "wij_willen_soep_en_ijsjes_aan_het_ijsselmeer"
+    }.each do |data,underscored|
+      assert_equal underscored, data.underscore
+    end
+  end
+
+  def test_titleize
+    {
+      "wij willen soep en ijsjes aan het IJsselmeer" => "Wij Willen Soep En IJsjes Aan Het IJsselmeer",
+      "WijWillenSoepEnIJsjesAanHetIJsselmeer"        => "Wij Willen Soep En IJsjes Aan Het IJsselmeer",
+      "MeerFreeBSDVoorIedereen"                      => "Meer Free Bsd Voor Iedereen",
+      "GebruikIMAlleenStiekem"                       => "Gebruik Im Alleen Stiekem",
+      "BahMJIsHelemaalNiets"                         => "Bah Mj Is Helemaal Niets"
+    }.each do |data,titleized|
+      assert_equal titleized, data.titleize
+    end
+  end
+
+  def test_camelize
+    {
+      "la_die_da"                                    => "LaDieDa",
+      "ijs_ijs_wij_willen_ijs"                       => "IJsIJsWijWillenIJs",
+      "wij_willen_soep_en_ijsjes_aan_het_ijsselmeer" => "WijWillenSoepEnIJsjesAanHetIJsselmeer"
+    }.each do |underscored,camelcase|
+      assert_equal camelcase, underscored.camelize
+      assert_equal underscored, camelcase.underscore
     end
   end
 end
