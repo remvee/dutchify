62df9914068f6488519467a695f7003a97e5004e
remco <remco@cb8d8d85-131c-0410-adf4-82ea522b99ed>
2007-06-19 19:44:47 +0000

    r3175@RemBook2:  remco | 2007-06-19 21:44:50 +0200  fixes voor rails 1.2.3
    git-svn-id: http://svn.remvee.net/plugins/dutchify@24 cb8d8d85-131c-0410-adf4-82ea522b99ed

--- lib/active_record_fixes.rb
+++ lib/active_record_fixes.rb
@@ -3,11 +3,15 @@ module ActiveRecord # :nodoc:
   module ConnectionAdapters # :nodoc:
     module Quoting # :nodoc:
       def quote(value, column = nil)
+        return value.quoted_id if value.respond_to?(:quoted_id)
+
         case value
-          when String
+          when String, ActiveSupport::Multibyte::Chars
+            value = value.to_s
             if column && column.type == :binary && column.class.respond_to?(:string_to_binary)
               "'#{quote_string(column.class.string_to_binary(value))}'" # ' (for ruby-mode)
-            elsif column && [:integer, :float].include?(column.type) 
+            elsif column && [:integer, :float].include?(column.type)
+              value = column.type == :integer ? value.to_i : value.to_f
               value.to_s
             else
               "'#{quote_string(value)}'" # ' (for ruby-mode)
@@ -16,6 +20,8 @@ module ActiveRecord # :nodoc:
           when TrueClass             then (column && column.type == :integer ? '1' : quoted_true)
           when FalseClass            then (column && column.type == :integer ? '0' : quoted_false)
           when Float, Fixnum, Bignum then value.to_s
+          # BigDecimals need to be output in a non-normalized form and quoted.
+          when BigDecimal               then value.to_s('F')
           when Date                  then "'#{value.strftime("%Y-%m-%d")}'"
           when Time, DateTime        then "'#{quoted_date(value)}'"
           else                            "'#{quote_string(value.to_yaml)}'"
