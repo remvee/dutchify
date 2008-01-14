7101a587a0d44e13330ff2d2592dd666687f276f
remco <remco@cb8d8d85-131c-0410-adf4-82ea522b99ed>
2008-01-14 12:56:36 +0000

    r3335@rembook2:  remco | 2007-09-12 21:20:48 +0200  sta meerdere objecten toe in error_messages_for
    git-svn-id: http://svn.remvee.net/plugins/dutchify@26 cb8d8d85-131c-0410-adf4-82ea522b99ed

--- lib/active_record_helper.rb
+++ lib/active_record_helper.rb
@@ -1,24 +1,54 @@
 module ActionView # :nodoc:
   module Helpers # :nodoc:
     module ActiveRecordHelper # :nodoc:
-      def error_messages_for(object_name, options = {})
-        options = options.symbolize_keys
-        object = instance_variable_get("@#{object_name}")
-        unless object.nil? || object.errors.empty?
-          human_name = object.class.respond_to?(:human_name) ? object.class.human_name : object.class.name
-          content_tag("div",
-            content_tag(
-              options[:header_tag] || "h2",
-              (object.errors.size == 1 ? 
-                  "Vanwege het volgende probleem kon #{human_name} niet opgeslagen worden." :
-                  "Vanwege de volgende problemen kon #{human_name} niet opgeslagen worden.")
-            ) +
-            (object.errors.size == 1 ? 
-                content_tag("p", "Er is een probleem met het volgende veld:") :
-                content_tag("p", "Er zijn problemen met de volgende velden:")) +
-            content_tag("ul", object.errors.full_messages.collect { |msg| content_tag("li", msg) }),
-            "id" => options[:id] || "errorExplanation", "class" => options[:class] || "errorExplanation" 
+      # Geef een string terug met een <tt>DIV</tt> met daarin alle foutmeldingen voor de objecten waarvan de instantie variabel namen
+      # gegeven zijn.  Als er meerder objecten gespecificieerd zijn, worden de foutmeldingen getoond in de volgorde dat de object
+      # namen door gegeven zijn.
+      #
+      # Deze <tt>DIV</tt> kan aangepast worden met de volgende opties:
+      #
+      # * <tt>header_tag</tt> - Gebruikt in de kop van de foutmeldingen div (standaard: h2)
+      # * <tt>id</tt> - Het id van de foutmeldingen div (standaard: errorExplanation)
+      # * <tt>class</tt> - De class van de foutmeldingen div (standaard: errorExplanation)
+      # * <tt>object_name</tt> - De object naam om te gebruiken in de kop, of een andere tekst.  If <tt>object_name</tt> gegeven
+      # wordt, wordt van het eerste object de <tt>human_name</tt> class methode geprobeerd anders wordt de eerste gegeven object
+      # naam gebruikt.
+      #
+      # Voorbeelden:
+      #
+      #   error_messages_for :user
+      #   error_messages_for :user, :address, :object_name => 'gebruiker'
+      def error_messages_for(*params)
+        options = Hash === params.last ? params.pop.symbolize_keys : {}
+        objects = params.collect {|object_name| instance_variable_get("@#{object_name}") }
+        count   = objects.compact.inject(0) {|sum, object| sum + object.errors.count }
+        unless count.zero?
+          html = {}
+          [:id, :class].each do |key|
+            if options.include?(key)
+              value = options[key]
+              html[key] = value unless value.blank?
+            else
+              html[key] = 'errorExplanation'
+            end
+          end
+          human_name = if options[:object_name]
+            options[:object_name]
+          elsif objects.first && objects.first.class.respond_to?(:human_name)
+            objects.first.class.human_name
+          else
+            params.first.to_s.gsub('_', ' ')
+          end
+          header_message = "Vanwege #{pluralize(count, 'probleem', 'problemen')} kan #{human_name} niet opgeslagen worden"
+          error_messages = objects.map {|object| object.errors.full_messages.map {|msg| content_tag(:li, msg) } }
+          content_tag(:div,
+            content_tag(options[:header_tag] || :h2, header_message) <<
+              content_tag(:p, count == 1 ? 'Er is een probleem met het volgende veld:' : 'Er zijn problemen met de volgende velden:') <<
+              content_tag(:ul, error_messages),
+            html
           )
+        else
+          ''
         end
       end
     end
--- test/active_record_helper_test.rb
+++ test/active_record_helper_test.rb
@@ -1,28 +1,80 @@
 require 'ostruct'
 require 'test_helper'
+
 require 'rails/version'
 require 'active_support'
 require 'active_record'
-# require 'action_view/helpers/date_helper'
-# require 'action_view/helpers/form_helper'
+require 'action_view/helpers/text_helper'
+require 'action_view/helpers/tag_helper'
+
 require File.dirname(__FILE__) + '/../lib/active_record_helper'
 
 class ActiveRecordHelperTest < Test::Unit::TestCase
+  include ActionView::Helpers::TextHelper
+  include ActionView::Helpers::TagHelper
   include ActionView::Helpers::ActiveRecordHelper
   
-  def test_error_messages_for_should_render_two_errors
-    @person = OpenStruct.new(:errors => OpenStruct.new(:size => 2, :full_messages => ['fout1', 'fout2']))
-    assert_match /fout1/, error_messages_for(:person)
-    assert_match /fout2/, error_messages_for(:person)
+  class RecordWithErrors
+    attr_reader :errors
+    def initialize(*errors)
+      @errors = errors
+      class << @errors
+        def count; size; end
+        def full_messages; self; end
+      end
+    end
+  end
+  
+  class RecordWithErrorsAndHumanName < RecordWithErrors
+    def self.human_name; 'RecordHumanName'; end
+  end
+  
+  RecordWithoutErrors = RecordWithErrors
+    
+  def test_error_messages_for_should_render_one_error
+    @person = RecordWithErrors.new 'fout'
+    assert_equal %q{<div class="errorExplanation" id="errorExplanation"><h2>Vanwege 1 probleem kan person niet opgeslagen worden</h2><p>Er is een probleem met het volgende veld:</p><ul><li>fout</li></ul></div>},
+        error_messages_for(:person)
+  end
+  
+  def test_error_messages_for_should_render_one_error_from_two_objects
+    @person = RecordWithErrors.new 'fout'
+    @address = RecordWithoutErrors.new
+    assert_equal %q{<div class="errorExplanation" id="errorExplanation"><h2>Vanwege 1 probleem kan person niet opgeslagen worden</h2><p>Er is een probleem met het volgende veld:</p><ul><li>fout</li></ul></div>},
+        error_messages_for(:person, :address)
+  end
+  
+  def test_error_messages_for_should_render_two_errors_from_one_object
+    @person = RecordWithErrors.new 'fout1', 'fout2'
+    assert_equal %q{<div class="errorExplanation" id="errorExplanation"><h2>Vanwege 2 problemen kan person niet opgeslagen worden</h2><p>Er zijn problemen met de volgende velden:</p><ul><li>fout1</li><li>fout2</li></ul></div>},
+        error_messages_for(:person)
+  end
+
+  def test_error_messages_for_should_render_two_errors_from_two_objects
+    @person = RecordWithErrors.new 'fout1'
+    @address = RecordWithErrors.new 'fout2'
+    assert_equal %q{<div class="errorExplanation" id="errorExplanation"><h2>Vanwege 2 problemen kan person niet opgeslagen worden</h2><p>Er zijn problemen met de volgende velden:</p><ul><li>fout1</li><li>fout2</li></ul></div>},
+        error_messages_for(:person, :address)
+  end
+  
+  def test_error_messages_for_should_render_object_name
+    @person = RecordWithErrors.new 'fout'
+    assert_equal %q{<div class="errorExplanation" id="errorExplanation"><h2>Vanwege 1 probleem kan persoon niet opgeslagen worden</h2><p>Er is een probleem met het volgende veld:</p><ul><li>fout</li></ul></div>},
+        error_messages_for(:person, :object_name => 'persoon')
+  end
+  
+  def test_error_messages_for_should_render_human_name
+    @person = RecordWithErrorsAndHumanName.new('fout')
+    
+    assert_equal %q{<div class="errorExplanation" id="errorExplanation"><h2>Vanwege 1 probleem kan RecordHumanName niet opgeslagen worden</h2><p>Er is een probleem met het volgende veld:</p><ul><li>fout</li></ul></div>},
+        error_messages_for(:person)
   end
   
   def test_error_messages_for_should_allow_nil
-    @some_nil_value = nil
-    assert_nil error_messages_for(:some_nil_value)
+    assert error_messages_for(:some_nil).blank?
   end
   
-private
-  def content_tag(name, content, options = {})
-    "<#{name} #{options.map{|k,v|"#{k}=#{v.inspect}"}.join(' ')}>#{content}</#{name}>"
+  def test_error_messages_for_should_allow_multiple_nils
+    assert error_messages_for(:some_nil, :some_other_nil).blank?
   end
 end
